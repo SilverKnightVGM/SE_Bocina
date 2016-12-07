9242ea7e7da29df97968e9b02812a7a54939b10e
Enzo <enzocasamassima@gmail.com>
2016-12-06 21:36:10 -0400

    -Everything working -No known mistakes for now

--- src/se_bocina/Interfaz.java
+++ src/se_bocina/Interfaz.java
@@ -8,7 +8,9 @@ package se_bocina;
 import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.Iterator;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -28,7 +30,7 @@ public class Interfaz extends javax.swing.JFrame {
 
     public Interfaz() {
         initComponents();
-
+        
         addRules();
 
         //Making Label Resizabble and centered
@@ -70,8 +72,11 @@ public class Interfaz extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         Positive = new javax.swing.JButton();
         Negative = new javax.swing.JButton();
+        Restart = new javax.swing.JButton();
+        About = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Sistema Experto IA 2016");
         getContentPane().setLayout(new java.awt.GridLayout(2, 0));
 
         Question.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
@@ -84,14 +89,14 @@ public class Interfaz extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(Question, javax.swing.GroupLayout.DEFAULT_SIZE, 282, Short.MAX_VALUE)
+                .addComponent(Question, javax.swing.GroupLayout.DEFAULT_SIZE, 353, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(Question, javax.swing.GroupLayout.DEFAULT_SIZE, 115, Short.MAX_VALUE)
+                .addComponent(Question, javax.swing.GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -113,24 +118,48 @@ public class Interfaz extends javax.swing.JFrame {
             }
         });
 
+        Restart.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        Restart.setText("Restart");
+        Restart.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RestartActionPerformed(evt);
+            }
+        });
+
+        About.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        About.setText("About");
+        About.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AboutActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(Restart, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(Positive, javax.swing.GroupLayout.DEFAULT_SIZE, 173, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(Positive, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(Negative, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 282, Short.MAX_VALUE))
+                    .addComponent(Negative, javax.swing.GroupLayout.DEFAULT_SIZE, 174, Short.MAX_VALUE)
+                    .addComponent(About, javax.swing.GroupLayout.DEFAULT_SIZE, 174, Short.MAX_VALUE))
                 .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(Positive, javax.swing.GroupLayout.DEFAULT_SIZE, 54, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(Negative, javax.swing.GroupLayout.DEFAULT_SIZE, 55, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(Positive, javax.swing.GroupLayout.DEFAULT_SIZE, 79, Short.MAX_VALUE)
+                    .addComponent(Negative, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(About, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(Restart, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
 
@@ -142,7 +171,7 @@ public class Interfaz extends javax.swing.JFrame {
     private void PositiveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PositiveActionPerformed
         // TODO add your handling code here:
 
-        if (pregunta_actual < (rules.size() - 1)) {
+        if (pregunta_actual < (rules.size())) {
             decisiones(1);
         } else {
             System.out.println("No hay mas preguntas");
@@ -153,13 +182,23 @@ public class Interfaz extends javax.swing.JFrame {
     private void NegativeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NegativeActionPerformed
         // TODO add your handling code here:
 
-        if (pregunta_actual < (rules.size() - 1)) {
+        if (pregunta_actual < (rules.size())) {
             decisiones(0);
         } else {
             System.out.println("No hay mas preguntas");
         }
 
     }//GEN-LAST:event_NegativeActionPerformed
+
+    private void RestartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RestartActionPerformed
+        // TODO add your handling code here:
+        restart();
+    }//GEN-LAST:event_RestartActionPerformed
+
+    private void AboutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AboutActionPerformed
+        // TODO add your handling code here:
+        showAbout();
+    }//GEN-LAST:event_AboutActionPerformed
 
     /**
      * @param args the command line arguments
@@ -197,9 +236,11 @@ public class Interfaz extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton About;
     private javax.swing.JButton Negative;
     private javax.swing.JButton Positive;
     private javax.swing.JLabel Question;
+    private javax.swing.JButton Restart;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
@@ -208,53 +249,208 @@ public class Interfaz extends javax.swing.JFrame {
 
         /**
          * Cadena_actual 1,6,7,8; 2,9,10,11; 3,7,12,13; 4,7,14,15; 5,16;
-         * pregunta_actual 0,5,6,7; 1,8, 9,10; 2,6,11,12; 3,6,13,14; 4,15;
+         * pregunta_actual 0,5,6,7; 1,8,9,10; 2,6,11,12; 3,6,13,14; 4,15;
          */
         valor_default = rules.get(pregunta_actual).getValor();
 
         switch (cadena_actual) {
             case 1:
                 System.out.println("Bloque 1");
+
                 if ((pregunta_actual == 0) && (valor_default == respuesta_dada)) {
-                    System.out.println("Primera Pregunta!");
+                    System.out.println("Pregunta: " + rules.indexOf(rules.get(pregunta_actual + 1)));
                     pregunta_actual = 5;
                     break;
                 } else if ((pregunta_actual == 0) && (valor_default != respuesta_dada)) {
                     cadena_actual++;
+                    pregunta_actual++;
                     break;
                 }
 
-                if ((pregunta_actual <= 8) && valor_default == respuesta_dada) {
+                if ((pregunta_actual <= 7) && valor_default == respuesta_dada) {
                     recomendacion = rules.get(pregunta_actual).getRespuesta();
                     Question.setText("<html><center>Recomendacion: " + recomendacion + "</center></html>");
                     System.out.println("END");
                     ended = true;
-                } else if (pregunta_actual > 7) {
-                    cadena_actual++;
-                    System.out.println("Cambiando Cadena");
+                    endScreen(1);
+                } else if (pregunta_actual > 6) {
+                    endScreen(0);
+                } else {
+                    pregunta_actual++;
                 }
 
                 break;
             case 2:
                 System.out.println("Bloque 2");
+
+                if ((pregunta_actual == 1) && (valor_default == respuesta_dada)) {
+                    System.out.println("Pregunta: " + rules.indexOf(rules.get(pregunta_actual + 1)));
+                    pregunta_actual = 8;
+                    break;
+                } else if ((pregunta_actual == 1) && (valor_default != respuesta_dada)) {
+                    cadena_actual++;
+                    pregunta_actual++;
+                    break;
+                }
+
+                if ((pregunta_actual <= 10) && valor_default == respuesta_dada) {
+                    recomendacion = rules.get(pregunta_actual).getRespuesta();
+                    Question.setText("<html><center>Recomendacion: " + recomendacion + "</center></html>");
+                    System.out.println("END");
+                    ended = true;
+                    endScreen(1);
+                } else if (pregunta_actual > 9) {
+                    ended = true;
+                    endScreen(0);
+                } else {
+                    pregunta_actual++;
+                }
+
                 break;
             case 3:
                 System.out.println("Bloque 3");
+
+                if ((pregunta_actual == 2) && (valor_default == respuesta_dada)) {
+                    System.out.println("Pregunta: " + rules.indexOf(rules.get(pregunta_actual + 1)));
+                    pregunta_actual = 6;
+                    break;
+                } else if ((pregunta_actual == 2) && (valor_default != respuesta_dada)) {
+                    cadena_actual++;
+                    pregunta_actual++;
+                    break;
+                }
+
+                if ((pregunta_actual <= 12) && valor_default == respuesta_dada) {
+                    recomendacion = rules.get(pregunta_actual).getRespuesta();
+                    Question.setText("<html><center>Recomendacion: " + recomendacion + "</center></html>");
+                    System.out.println("END");
+                    ended = true;
+                    endScreen(1);
+                } else if (pregunta_actual > 12) {
+                    ended = true;
+                    endScreen(0);
+                } else {
+                    if (pregunta_actual == 6) {
+                        pregunta_actual = 11;
+                    } else if (pregunta_actual == 11) {
+                        pregunta_actual = 12;
+                    }
+                }
+
                 break;
             case 4:
                 System.out.println("Bloque 4");
+
+                if ((pregunta_actual == 3) && (valor_default == respuesta_dada)) {
+                    System.out.println("Pregunta: " + rules.indexOf(pregunta_actual));
+                    pregunta_actual = 6;
+                    break;
+                } else if ((pregunta_actual == 3) && (valor_default != respuesta_dada)) {
+                    cadena_actual++;
+                    pregunta_actual++;
+                    break;
+                }
+
+                if ((pregunta_actual <= 14) && valor_default == respuesta_dada) {
+                    recomendacion = rules.get(pregunta_actual).getRespuesta();
+                    Question.setText("<html><center>Recomendacion: " + recomendacion + "</center></html>");
+                    System.out.println("END");
+                    ended = true;
+                    endScreen(1);
+                } else if (pregunta_actual > 14) {
+                    ended = true;
+                    endScreen(0);
+                } else {
+                    if (pregunta_actual == 6) {
+                        pregunta_actual = 13;
+                    } else if (pregunta_actual == 13) {
+                        pregunta_actual = 14;
+                    }
+                }
+
                 break;
             case 5:
                 System.out.println("Bloque 5");
+
+                if ((pregunta_actual == 4) && (valor_default == respuesta_dada)) {
+                    System.out.println("Pregunta: " + rules.indexOf(pregunta_actual));
+                    pregunta_actual = 15;
+                    break;
+                } else if ((pregunta_actual == 4) && (valor_default != respuesta_dada)) {
+                    ended = true;
+                    endScreen(0);
+                    break;
+                }
+
+                if ((pregunta_actual <= 15) && valor_default == respuesta_dada) {
+                    recomendacion = rules.get(pregunta_actual).getRespuesta();
+                    Question.setText("<html><center>Recomendacion: " + recomendacion + "</center></html>");
+                    System.out.println("END");
+                    ended = true;
+                    endScreen(1);
+                } else if (pregunta_actual > 15) {
+                    ended = true;
+                    endScreen(0);
+                }
+
                 break;
         }
 
         if (ended == false) {
             //Avanzar a la siguiente pregunta
-            pregunta_actual++;
             Question.setText("<html><center>" + rules.get(pregunta_actual).getPregunta() + "?</center></html>");
-        }else{
+        } else {
             System.out.println("Eso es todo!");
         }
+    }
+
+    private void endScreen(int exitCode) {
+        Positive.setEnabled(false);
+        Negative.setEnabled(false);
+
+        String exitString = "";
+
+        if (exitCode == 1) {
+            return;
+        } else if (exitCode == 0) {
+            exitString = "No pudimos encontrar su problema. Intente de nuevo o contacte al vendedor.";
+        } else {
+            exitString = "Hubo un error";
+        }
+        Question.setText("<html><center>Recomendacion: " + exitString + "</center></html>");
+    }
+
+    private void restart() {
+        pregunta_actual = 0;
+        cadena_actual = 1;
+        valor_default = -1;
+        recomendacion = null;
+        ended = false;
+
+        Positive.setEnabled(true);
+        Negative.setEnabled(true);
+
+        //Making Label Resizabble and centered
+        Question.setText("<html><center>" + rules.get(pregunta_actual).getPregunta() + "?</center></html>");
+        Question.setPreferredSize(new Dimension(1, 1));
+    }
+
+    private void showAbout() {
+        // show a joptionpane dialog using showMessageDialog
+        JOptionPane.showMessageDialog(rootPane,
+                "<html><center><h3>"
+                + "<b>Sistema experto para detectar fallas en: <br></br>"
+                + "<p style=\"color:blue;\">"
+                + "AmazonBasics Portable Bluetooth Speaker</p></h3></b></center>"
+                + "<h4>Hecho por:<ul>"
+                + "<li>Enzo Casamassima (14-0696)</li>"
+                + "<li>Ariel Rosario (14-0333)</li>"
+                + "</ul></h4></center></html>",
+                "About",
+                JOptionPane.INFORMATION_MESSAGE);
+//        JOptionPane optionPane = new JOptionPane(new JLabel("Hello World", JLabel.CENTER));
+//        JDialog dialog = optionPane.createDialog("");
+//        dialog.setModal(true);
+//        dialog.setVisible(true);
     }
 }
--- src/se_bocina/SE_Bocina.java
+++ src/se_bocina/SE_Bocina.java
@@ -28,53 +28,59 @@ public class SE_Bocina {
      */
     public static void main(String[] args) {
 
-        String fileName = "input.txt";
-
-        try {
-//            Scanner input = new Scanner(new BufferedReader(new FileReader(fileName)));
-            Scanner input = new Scanner(new File(fileName), "Unicode");
-            input.useDelimiter("\\s*;\\s*|\n");
-            ArrayList<Rule> test = new ArrayList<>();
-
-            while (input.hasNext()) {
-                int id = input.nextInt();
-                String pregunta = input.next();
-                int valor = input.nextInt();
-                test.add(new Rule(pregunta, valor, Integer.toString(id)));
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Interfaz().setVisible(true);
             }
+        });
 
-            for (int i = 0; i < test.size(); i++) {
-//                System.out.println(test.get(i).toString());
-            }
-
-            System.out.println(test.get(1).getPregunta() + "?");
-            Rule ru = new Rule("Hola1", 1, test);
-            System.out.println(ru.getOpciones().get(1).getPregunta());
-            input.close();
-
-        } catch (FileNotFoundException | InputMismatchException ex) {
-//            Logger.getLogger(SE_Bocina.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.print(ex.getMessage());
-            ex.printStackTrace();
-        }
-
+//        String fileName = "input.txt";
+//
+//        try {
+////            Scanner input = new Scanner(new BufferedReader(new FileReader(fileName)));
+//            Scanner input = new Scanner(new File(fileName), "Unicode");
+//            input.useDelimiter("\\s*;\\s*|\n");
+//            ArrayList<Rule> test = new ArrayList<>();
+//
+//            while (input.hasNext()) {
+//                int id = input.nextInt();
+//                String pregunta = input.next();
+//                int valor = input.nextInt();
+//                test.add(new Rule(pregunta, valor, Integer.toString(id)));
+//            }
+//
+//            for (int i = 0; i < test.size(); i++) {
+////                System.out.println(test.get(i).toString());
+//            }
+//
+//            System.out.println(test.get(1).getPregunta() + "?");
+//            Rule ru = new Rule("Hola1", 1, test);
+//            System.out.println(ru.getOpciones().get(1).getPregunta());
+//            input.close();
+//
+//        } catch (FileNotFoundException | InputMismatchException ex) {
+////            Logger.getLogger(SE_Bocina.class.getName()).log(Level.SEVERE, null, ex);
+//            System.out.print(ex.getMessage());
+//            ex.printStackTrace();
+//        }
 //        Rule R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14, R15, R16;
 //        ArrayList<Rule> rules = new ArrayList<>();
 //        rules.add(R1);
     }
 
-    public static void examples1() {
-        Tree<String> tree = new Tree<>("Root");
-        tree.addLeaf("A");
-        tree.addLeaf("A", "B");
-        tree.addLeaf("A", "C");
-        tree.addLeaf("B", "D");
-        tree.addLeaf("E", "D");
-        tree.addLeaf("X");
-
-        System.out.println(tree.toString());
-        System.out.println("--------------");
-        System.out.println(tree.getSuccessors("Root"));
-        System.out.println(tree.getSuccessors("E"));
-    }
+//    public static void examples1() {
+//        Tree<String> tree = new Tree<>("Root");
+//        tree.addLeaf("A");
+//        tree.addLeaf("A", "B");
+//        tree.addLeaf("A", "C");
+//        tree.addLeaf("B", "D");
+//        tree.addLeaf("E", "D");
+//        tree.addLeaf("X");
+//
+//        System.out.println(tree.toString());
+//        System.out.println("--------------");
+//        System.out.println(tree.getSuccessors("Root"));
+//        System.out.println(tree.getSuccessors("E"));
+//    }
 }
